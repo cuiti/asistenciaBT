52fc9d8512bb084752f1fabff956f3365850c239
Julia Lasarte <julia.lasarte@gmail.com>
2015-04-26 20:54:27 -0300

    agregado ratchet y modificada funcion de busqueda para que liste los disp no inscriptos

--- platforms/android/assets/www/js/bluetooth.js
+++ platforms/android/assets/www/js/bluetooth.js
@@ -9,32 +9,62 @@
         ko.applyBindings(new AsistenciaViewModel());
     }
 
+    function AlumnoViewModel(data) {
+        var self = this;
+        self.name = "";
+        self.device_mac = "";
+        self.present = ko.observable(false);
+        
+        if (data) {
+            self.name = data.name;
+            self.device_mac = data.device_mac;
+            self.present(data.present);
+        }
+    }
+
 	function AsistenciaViewModel() {
         var self = this;
-
+        self.detectedDevices = ko.observableArray([]);
         self.alumnos = ko.observableArray([
-            {name:"Juan Fernández", device_mac:"0:5F:2E:BD:32:DC", present: ko.observable(false)},
-        	{name:"Alfonso Cuitiño", device_mac : "00:08:CA:3E:D5:89", present: ko.observable(false)},
-        	{name:"Julia Lasarte", device_mac : "54:44:08:CA:BC:28", present: ko.observable(false)},
-        	{name:"Martin Couso", device_mac : "", present: ko.observable(false)},
-        	{name:"Andres Pinto", device_mac : "", present: ko.observable(false)}
-        	
+            new AlumnoViewModel({name:"Juan Fernández", device_mac:"0:5F:2E:BD:32:DC", present: false}),
+            new AlumnoViewModel({name:"Alfonso Cuitiño", device_mac : "00:08:CA:3E:D5:89", present: false}),
+            new AlumnoViewModel({name:"Julia Lasarte", device_mac : "54:44:08:CA:BC:28", present: false}),
+            new AlumnoViewModel({name:"Martin Couso", device_mac : "", present: false}),
+        	new AlumnoViewModel({name:"Andres Pinto", device_mac : "", present: false})
         ]);
+        
+        self.registerStudent = function(studentDevice) {
+            self.alumnos.push(studentDevice);
+            self.detectedDevices.remove(studentDevice);
+        };
+        
         
         self.scanDevices = function() {
             BC.bluetooth.addEventListener("newdevice",self.add);
             BC.Bluetooth.OpenBluetooth(function(){alert("todo ok!");},function(){alert("bluetooth open error!");});
             BC.Bluetooth.StartScan();//start hybrid scan,in android platform the default scan strategy is scan for BLE device 5s first, then scan for classical device 12s,and restart the hybrid scan untill the BC.Bluetooth.StopScan be called. in IOS platform only start the LE scan.
-	   }
+	   };
 	
         self.add = function(s){
         	var newDevice = s.target;	
 			//funcion que busca la direccion dentro del array
-			ko.utils.arrayForEach(self.alumnos(), function(item) {
-		   		if (newDevice.deviceAddress == item.device_mac) {
-		   			item.present(true);
-		    	}
-		    });
-			var newDevice = BC.bluetooth.devices[deviceAddress];
+            var in_students = ko.utils.arrayFirst(self.alumnos(),
+                        function(item) {
+                            return newDevice.deviceAddress == item.device_mac;
+                        });
+            
+            var in_detected_devices = ko.utils.arrayFirst(self.detectedDevices(),
+                        function(item) {
+                            return newDevice.deviceAddress == item.device_mac;
+                        });
+            
+            if (in_students) {
+                in_students.present(true);
+            } else {
+                if (!in_detected_devices) {
+                    self.detectedDevices.push(new AlumnoViewModel({name:newDevice.deviceName,device_mac:newDevice.deviceAddress,present:false}))
+                }
+            }
+
         }
     }
