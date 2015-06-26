7fa00a3e39f04e2ebcb6f5258eea49f0776d9ae0
Alfonso Cuitiño <alfonso_cuiti@hotmail.com>
2015-06-25 22:25:33 -0300

    conexion con envio de rfcommWrite()

--- www/js/bluetooth.js
+++ www/js/bluetooth.js
@@ -2,14 +2,13 @@
     // Wait for device API libraries to load
     //
  document.addEventListener("deviceready",onDeviceReady,false);
- document.write("<script type='text/javascript' src='cordova.js'></script>");
 
     // device APIs are available
     //
     function onDeviceReady() {
         ko.applyBindings(new AsistenciaViewModel());
     }
-
+					
 function getUserMac(){
     var string = window.device.uuid;
     return string;
@@ -29,7 +28,7 @@ function getUserMac(){
 	function AsistenciaViewModel() {
         var self = this;
         self.detectedDevices = ko.observableArray([]);
-        self.teachers = ko.observableArray([new AlumnoViewModel({name:"Profesor prueba", device_mac:"78:59:5E:9A:F5:E3", present: false})]);
+        self.teachers = ko.observableArray([new AlumnoViewModel({name:"Profesor prueba", device_mac:"AC:22:0B:35:EE:4E", present: false})]);
         self.alumnos = ko.observableArray([
             new AlumnoViewModel({name:"Juan Fernández", device_mac:"00:5F:2E:BD:32:DC", present: false}),
             new AlumnoViewModel({name:"Alfonso Cuitiño", device_mac : "", present: false}),
@@ -59,11 +58,18 @@ function getUserMac(){
                 in_teachers.present(true);
                 alert("profesor presente!!!"+ device.deviceAddress + device.deviceName);
                 //var deviceT = BC.bluetooth.devices[device.deviceAddress];
-                device.connect(function(){alert("device is already connected well!");},function(){alert("no se conecta el puto UUID");},"7A9C3B55-78D0-44A7-A94E-A93E3FE118CE",true);
-                /*device.createPair(function(mes){alert("create pair with device success!");}, function(){alert("no me parie");});
+                device.connect(function(){
+								device.rfcommWrite("Hex","01",function(data){console.log(data);});
+								//device.prepare(function(){alert("device prepared");},function(message){alert(message);});
+								alert("device is already connected well!");},
+							function(e){alert("no se conecta el puto UUID");console.log(e);},"7A9C3B55-78D0-44A7-A94E-A93E3FE118CE",true);
+                
+										
+					
+				/*device.createPair(function(mes){alert("create pair with device success!");}, function(){alert("no me parie");});
                 device.prepare(function(){alert("hola todo bien prepare");},function(){alert("todo mal prepare");});
                 device.discoverServices(function(){alert("hola todo bien disco");}, function(){alert("todo mal servicesdisco");});
-               */ alert(device.services);
+               //alert(device.services);
             /*for (i=0; i<device.services.length; i++){
                 alert(device.services[i].name);
               alert(device.services[i].uuid);
@@ -73,7 +79,7 @@ function getUserMac(){
                //device.createPair(function(mes){alert("create pair with device success!")}, function(){alert("no me parie");});
                //device.createPair(function(mes){alert("create pair with device success!")});
             }
-            else alert(device.deviceName);
+            //else alert(device.deviceName);
         };
 
         self.registerStudent = function(studentDevice) {
@@ -94,12 +100,12 @@ function getUserMac(){
 			//funcion que busca la direccion dentro del array
             var in_students = ko.utils.arrayFirst(self.alumnos(),
                         function(item) {
-                            return newDevice.deviceAddress == item.device_mac;
+                            return newDevice.deviceAddress.toUpperCase() == item.device_mac.toUpperCase();
                         });
             
             var in_detected_devices = ko.utils.arrayFirst(self.detectedDevices(),
                         function(item) {
-                            return newDevice.deviceAddress == item.device_mac;
+                            return newDevice.deviceAddress.toUpperCase() == item.device_mac.toUpperCase();
                         });
             
             if (in_students) {
